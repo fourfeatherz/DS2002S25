59ffd4ac792abcb25f777221418b25107e815b37
fourfeatherz <jasonw@virginia.edu>
2025-04-28 11:47:54 -0400

    Create discord_basic.py

---
+++ python/bots/discord_basic.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents, case_insensitive=True)
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.name}! 👋")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Uh-oh, I don't know that command! Try `!hello`.")
+
+bot.run('YOUR_KEY')
